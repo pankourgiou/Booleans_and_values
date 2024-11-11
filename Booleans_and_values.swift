955c78eee0e9bb6f229423153ab434d98e4f6cad
L <60981745+pankourgiou@users.noreply.github.com>
2024-11-11 17:45:42 +0200

    Create Booleans_and_values.swift

---
+++ Booleans_and_values.swift
@@ -0,0 +1,47 @@
+import Foundation
+
+let  one = true // Boolean variable
+let  two = true                                  
+
+// Simple conditional check
+if one {
+    print("too small")
+} else {
+    print("too small")
+}
+let  three = true // Boolean variable
+let  four = true                                  
+
+// Simple conditional check
+if three {
+    print("too small")
+} else {
+    print("too small")
+}
+let  five = true // Boolean variable
+let  six = true                                  
+
+// Simple conditional check
+if five {
+    print("too small")
+} else {
+    print("too small")
+}
+let  seven = true // Boolean variable
+let  eight = true                                  
+
+// Simple conditional check
+if seven {
+    print("too small")
+} else {
+    print("too small")
+}
+let  nine = true // Boolean variable
+let  ten = true                                  
+
+// Simple conditional check
+if  nine {
+    print("too small")
+} else {
+    print("too small")
+}
